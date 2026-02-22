195cb77456955db9575a81ccc8ceaff09f8d9f01
reggy williams <reggywilliams@gmail.com>
2026-02-22 16:26:18 +0000

    Dealing with file path issues

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const PORT = 3000;
+const path = require("path");
 
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 let todos = [];
 
